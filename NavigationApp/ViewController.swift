66167ade19ee4eaa021896ee488631a9ae4d92ea
olzhs <oleg1984@mail.ru>
2022-01-21 23:29:22 +0500

    change ViewController (programming change scene in Navigation controller)

--- NavigationApp/ViewController.swift
+++ NavigationApp/ViewController.swift
@@ -11,9 +11,37 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func onGreen(_ sender: UIButton) {
+    
+        let nextViewController = storyboardInstance.instantiateViewController(withIdentifier: "greenViewController")
+        self.navigationController?.pushViewController(nextViewController, animated: true)
+        
+    }
+    
+    @IBAction func onYellow(_ sender: UIButton) {
+    
+        let nextViewController = storyboardInstance.instantiateViewController(withIdentifier: "yellowViewController")
+        self.navigationController?.pushViewController(nextViewController, animated: true)
+    }
+    
+    @IBAction func onMainScene(_ sender: UIButton) {
+   
+//        let mainViewController = storyboardInstance.instantiateViewController(withIdentifier: "redViewController")
+        self.navigationController?.popToRootViewController(animated: true)
+    
+    }
+    
+    @IBAction func toPreviosScene(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    let storyboardInstance = UIStoryboard(name: "Main", bundle: nil)
+
+    
+    
 
 }
 
